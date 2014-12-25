eba3e28945e568647ea425f779937496c94ab16b
LorDClockaN <davor@losinj.com>
2014-12-25 16:13:13 +0100

    FWB: Hidden Shit
    Change-Id: Icbbcdabfc37dfd9e90637c57a58ecb4e5a239724

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6329,6 +6329,12 @@ public final class Settings {
         public static final String ALLOW_LIGHTS = "allow_lights";
 
         /**
+         * Hidden shit
+         * @hide
+         */
+        public static final String HIDDEN_SHIT = "hidden_shit";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
