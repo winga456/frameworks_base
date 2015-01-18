03343afe78ebb4bc27534df5053139925edb9b69
maxwen <max.weninger@gmail.com>
2015-01-18 12:54:29 -0500

    Allow unlinking ringer with notification volume [1/2]
    Change-Id: Iafbb483858ca782e8a8ff13131c5a69ae1af0c61

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5462,6 +5462,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCK_SCREEN = "double_tap_sleep_lock_screen";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
