842c105b3d032335d281c03ca9629fb070c19df4
Steve Kondik <steve@cyngn.com>
2015-11-07 11:41:21 -0800

    MediaRecorder: Add pause interface
    - add pause in java level
    - add pause in jni level
    
    CRs-Fixed: 587051
    
    Add 2 APIs (suspend/resume) in MediaPlayer
    
    - API:suspend() will just pause the player and release all the decoders
      to replace release() which will release the whole player
    - API:resume() will just init the decoders again,
      then start() will be called to start to play the streaming
    
    Change-Id: Ia0c38bd827f21261b2bd87c04778e86362003344
    
    Conflicts:
    	media/java/android/media/MediaRecorder.java
    	media/jni/android_media_MediaRecorder.cpp

--- media/jni/android_media_MediaRecorder.cpp
+++ media/jni/android_media_MediaRecorder.cpp
@@ -40,8 +40,8 @@
 
 #include <system/audio.h>
 #include <android_runtime/android_view_Surface.h>
-
 #include "SeempLog.h"
+
 // ----------------------------------------------------------------------------
 
 using namespace android;
@@ -393,6 +393,14 @@ android_media_MediaRecorder_start(JNIEnv *env, jobject thiz)
 }
 
 static void
+android_media_MediaRecorder_pause(JNIEnv *env, jobject thiz)
+{
+    ALOGV("pause");
+    sp<MediaRecorder> mr = getMediaRecorder(env, thiz);
+    process_media_recorder_call(env, mr->pause(), "java/lang/RuntimeException", "pause failed.");
+}
+
+static void
 android_media_MediaRecorder_stop(JNIEnv *env, jobject thiz)
 {
     ALOGV("stop");
@@ -529,6 +537,7 @@ static JNINativeMethod gMethods[] = {
     {"getSurface",           "()Landroid/view/Surface;",        (void *)android_media_MediaRecorder_getSurface},
     {"getMaxAmplitude",      "()I",                             (void *)android_media_MediaRecorder_native_getMaxAmplitude},
     {"start",                "()V",                             (void *)android_media_MediaRecorder_start},
+    {"pause",                "()V",                             (void *)android_media_MediaRecorder_pause},
     {"stop",                 "()V",                             (void *)android_media_MediaRecorder_stop},
     {"native_reset",         "()V",                             (void *)android_media_MediaRecorder_native_reset},
     {"release",              "()V",                             (void *)android_media_MediaRecorder_release},
