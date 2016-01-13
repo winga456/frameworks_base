43ae7f37252af43cee411ad8a9418280bf3085f9
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2016-01-13 15:29:05 +0100

    Revert "OnTheGo: Add back removed setting"
    This reverts commit 66b55c2c4482b23f4bb03c522ccbf4e8874da172.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5945,13 +5945,6 @@ public final class Settings {
         public static final String OVERRIDE_CUSTOM_COLORS = "override_custom_colors";
 
         /**
-         * If On-The-Go should be displayed at the power menu.
-         *
-         * @hide
-         */
-        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
-
-        /**
          * The alpha value of the On-The-Go overlay.
          *
          * @hide
