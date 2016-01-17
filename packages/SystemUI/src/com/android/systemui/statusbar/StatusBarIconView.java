fdfc21fa1c7cc4db3295ec19495631c1c25fa628
rogersb11 <brettrogers11@gmail.com>
2016-01-17 01:09:19 -0500

    Correct null check

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -222,13 +222,15 @@ public class StatusBarIconView extends AnimatedImageView {
      *         be found
      */
     public static Drawable getIcon(Context context, StatusBarIcon icon) {
-        int userId = icon.user.getIdentifier();
+        int userId;
         if (icon != null) {
+            userId = icon.user.getIdentifier();
             if (userId == UserHandle.USER_ALL) {
                 userId = UserHandle.USER_OWNER;
             }
+            return icon.icon.loadDrawableAsUser(context, userId);
         }
-        return icon.icon.loadDrawableAsUser(context, userId);
+        return null;
     }
 
     public StatusBarIcon getStatusBarIcon() {
