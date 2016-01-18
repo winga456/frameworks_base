18a9eab62016adcaa3f8ca21616bc1b93db7c23b
beanstown106 <nbenis106@gmail.com>
2016-01-18 20:02:34 +0100

    Longpress power lockscreen torch [1/2]
    Long pressing power while screen is off will launch torch
    
    Credits:
    Lion0738: the main hooks here:https://github.com/lion0738/android_frameworks_base/commit/9af2b7844a4d973c8b6c542d7937f56a24a7e5f1
    Atlantis: the logic on where to hook into this for power button only
    AlexCruz: for helping and giving me some pointers along the way
    BeansTown106: for having to deal with smali, Yuck!! and cleaning up the code to properly only activate while screen is off
    
    Not really sure where authorship belongs here.. since this was peiced toghether from different sources.
    
    Oh Ya... OpenSourceSoftware FTW!!!
    
    Change-Id: I481e604fe89bb73c5afa66203d74becadbabe017
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6335,6 +6335,12 @@ public final class Settings {
         public static final String HIDDEN_SHIT = "hidden_shit";
 
         /**
+         * whether to enable torch on lockscreen
+         * @hide
+         */
+        public static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
