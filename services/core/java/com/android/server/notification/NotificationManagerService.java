DirtyUnicorns/android_frameworks_base

Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)
This allows the user to prevent the notification lights from showing during Zen mode as per Android 5.0.x

Change-Id: Ic91c855b42471a7b2620d7eb98dbbdc478e76196

services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1047,6 +1047,7 @@ public class NotificationManagerService extends SystemService {
             mDisableNotificationEffects = true;
         }
         mZenModeHelper.initZenMode();
+        mZenModeHelper.readLightsAllowedModeFromSetting();
         mInterruptionFilter = mZenModeHelper.getZenModeListenerInterruptionFilter();
 
         mUserProfiles.updateCache(getContext());
@@ -2583,7 +2584,8 @@ public class NotificationManagerService extends SystemService {
         // light
         // release the light
         boolean wasShowLights = mLights.remove(record.getKey());
-        final boolean canInterruptWithLight = aboveThreshold || isLedNotificationForcedOn(record);
+        final boolean canInterruptWithLight = canInterrupt || isLedNotificationForcedOn(record)
+                || (!canInterrupt && mZenModeHelper.getAreLightsAllowed());
         if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != 0 && canInterruptWithLight) {
             mLights.add(record.getKey());
             updateLightsLocked();

winga456/frameworks_base

services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1060,6 +1060,7 @@ public class NotificationManagerService extends SystemService {
             mDisableNotificationEffects = true;
         }
         mZenModeHelper.initZenMode();
+        mZenModeHelper.readLightsAllowedModeFromSetting();
         mInterruptionFilter = mZenModeHelper.getZenModeListenerInterruptionFilter();
 
         mUserProfiles.updateCache(getContext());
@@ -2631,7 +2632,8 @@ public class NotificationManagerService extends SystemService {
         // light
         // release the light
         boolean wasShowLights = mLights.remove(record.getKey());
-        final boolean aboveThresholdWithLight = aboveThreshold || isLedNotificationForcedOn(record);
+        final boolean aboveThresholdWithLight = aboveThreshold || isLedNotificationForcedOn(record)
+                || (!aboveThreshold && mZenModeHelper.getAreLightsAllowed());
         if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != 0 && aboveThresholdWithLight) {
             mLights.add(record.getKey());
             updateLightsLocked();