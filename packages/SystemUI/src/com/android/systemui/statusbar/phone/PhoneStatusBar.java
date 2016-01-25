78898c9750bcb52d632c575ea902c3bca40aa491
lion0738 <lion0738@naver.com>
2016-01-25 17:07:14 +0900

    SystemUI: Don't update if HeadsUp is disabled
    Change-Id: I0bd2875836929d6941cbec83762292a5a2c31572

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3438,6 +3438,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     protected void updateHeadsUp(String key, Entry entry, boolean shouldInterrupt,
             boolean alertAgain) {
+        if (!mUseHeadsUp) return;
         final boolean wasHeadsUp = isHeadsUp(key);
         if (wasHeadsUp) {
             if (!shouldInterrupt) {
