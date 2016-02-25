ChameleonOS/android_frameworks_base

Identicons: Add identicons to ChaOS Lab [1/3]
When enabled, new contacts will be assigned a unique
5x5 identicon as the contact photo instead of the boring
default one.

PS2:
Make Identicon an abstract class so we can use it to
create different types of identicons.  For starters
we'll use the github style in GithubIdenticon.  This
opens up the door to other types.

PS3:
Season with some salt.

PS4:
Use PNG format and tag it using an iTXt chunk before the
IEND to tag it with "identicon_marker" indicating it is
an identicon.  We can use this to identify if a photo is
an identicon and if so do what we want with it, like
removing identicons.

PS5:
We'll stick with JPG format and add a comment block to
the end.

PS6:
Better...stronger...faster.

PS7:
Add the dot matrix identicon

PS8:
Rename PolkaDotsIdenticon to DotMatrixIdenticon

PS9:
Clean up for release and add proper ChaOS Lab annotations.

Change-Id: I95fdca30686a2993ba5c2a9024a3cf5935b5d360

core/java/com/android/internal/util/chaos/identicons/DotMatrixIdenticon.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.chaos.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextUtils;
+
+import java.security.MessageDigest;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class DotMatrixIdenticon extends Identicon {
+    @Override
+    public Bitmap generateIdenticonBitmap(byte[] hash) {
+        if (hash.length < 16)
+            return null;
+
+        Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawColor(mBackgroundColor);
+
+        int blue = (hash[13] & 0x01f) << 3;
+        int green = (hash[14] & 0x01f) << 3;
+        int red = (hash[15] & 0x01f) << 3;
+        int color = Color.rgb(red, green, blue);
+        if (getColorDistance(color, mBackgroundColor) <= 64.0) {
+            color = getComplementaryColor(color);
+        }
+
+        Paint p = new Paint();
+        p.setColor(color);
+        p.setStyle(Paint.Style.FILL_AND_STROKE);
+        p.setAntiAlias(true);
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+                final int index = y * 5 + x;
+                float radius;
+                if ((index & 1) == 0) {
+                    radius = hash[index/2] & 0x0F;
+                } else {
+                    radius = (hash[index/2] >> 4) & 0x0F;
+                }
+                canvas.drawCircle(x * 16 + 8, y * 16 + 8, radius, p);
+            }
+        }
+        return bmp;
+    }
+
+    @Override
+    public byte[] generateIdenticonByteArray(byte[] hash) {
+        return bitmapToByteArray(generateIdenticonBitmap(hash));
+    }
+
+    @Override
+    public Bitmap generateIdenticonBitmap(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonBitmap(generateHash(saltedKey(key)));
+    }
+
+    @Override
+    public byte[] generateIdenticonByteArray(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonByteArray(generateHash(saltedKey(key)));
+    }
+}
+

core/java/com/android/internal/util/chaos/identicons/IdenticonFactory.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.chaos.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.provider.Settings;
+
+/**
+ * A factory to instantiate an identicon based on the type
+ */
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonFactory {
+
+    public static final int IDENTICON_STYLE_RETRO = 0;
+    public static final int IDENTICON_STYLE_CONTEMPORARY = 1;
+    public static final int IDENTICON_STYLE_SPIROGRAPH = 2;
+    public static final int IDENTICON_STYLE_DOTMATRIX = 3;
+
+    /**
+     * Get the appropriate identicon class based on the type passed in
+     * @param type
+     * @return
+     */
+    public static Identicon makeIdenticon(int type) {
+        switch (type) {
+            case IDENTICON_STYLE_RETRO:
+                return new RetroIdenticon();
+            case IDENTICON_STYLE_CONTEMPORARY:
+                return new NineBlockIdenticon();
+            case IDENTICON_STYLE_SPIROGRAPH:
+                return new SpirographIdenticon();
+            case IDENTICON_STYLE_DOTMATRIX:
+                return new DotMatrixIdenticon();
+            default:
+                throw new IllegalArgumentException("Unkown identicon type.");
+        }
+    }
+
+    /**
+     * Get the appropriate identicon class by looking up the user selected
+     * setting.
+     * @param context
+     * @return
+     */
+    public static Identicon makeIdenticon(Context context) {
+        return makeIdenticon(Settings.System.getInt(context.getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, 0));
+    }
+}
+

core/java/com/android/internal/util/chaos/identicons/RetroIdenticon.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.chaos.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class RetroIdenticon extends Identicon {
+
+    /**
+     * Generates a 5x5 identicon bitmap using the provided hash
+     * @param hash A 16 byte hash used to generate the identicon
+     * @return The bitmap of the identicon created
+     */
+    @Override
+    public Bitmap generateIdenticonBitmap(byte[] hash) {
+        if (hash.length != 16)
+            return null;
+
+        Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawColor(mBackgroundColor);
+        int color = generateColor(hash);
+        if (getColorDistance(color, mBackgroundColor) <= 64.0) {
+            color = getComplementaryColor(color);
+        }
+        Paint p = new Paint();
+        p.setColor(color);
+        p.setStyle(Paint.Style.FILL_AND_STROKE);
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 3; x++) {
+                final int index = y * 3 + x;
+                if (isOddParity(hash[index])) {
+                    drawSquare(canvas, x, y, 16, p);
+                    if (x == 0) drawSquare(canvas, 4, y, 16, p);
+                    if (x == 1) drawSquare(canvas, 3, y, 16, p);
+                }
+            }
+        }
+        return bmp;
+    }
+
+    /**
+     * Generates a 5x5 identicon bitmap, as a byte array, using the provided hash
+     * @param hash A 16 byte hash used to generate the identicon
+     * @return The bitmap byte array of the identicon created
+     */
+    @Override
+    public byte[] generateIdenticonByteArray(byte[] hash) {
+        return bitmapToByteArray(generateIdenticonBitmap(hash));
+    }
+
+    /**
+     * Generates an identicon bitmap using the provided key to generate a hash
+     * @param key A non empty string used to generate a hash when creating the identicon
+     * @return The bitmap of the identicon created
+     */
+    @Override
+    public Bitmap generateIdenticonBitmap(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonBitmap(generateHash(saltedKey(key)));
+    }
+
+    /**
+     * Generates an identicon bitmap, as a byte array, using the provided key to generate a hash
+     * @param key A non empty string used to generate a hash when creating the identicon
+     * @return The bitmap byte array of the identicon created
+     */
+    @Override
+    public byte[] generateIdenticonByteArray(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonByteArray(generateHash(saltedKey(key)));
+    }
+
+    private static boolean isOddParity(final byte b) {
+        int bb = b;
+        int bitCount = 0;
+        for (int i = 0; i < 8; i++, bb >>= 1) {
+            if ((bb & 1) != 0) {
+                bitCount++;
+            }
+        }
+
+        return (bitCount & 1) != 0;
+    }
+
+    private static int generateColor(byte[] hash) {
+        if (hash.length != 16)
+            return 0xFF000000;
+        final int r = hash[15] + hash[4] + hash[13] + hash[2] + hash[11] / 5;
+        final int g = hash[10] + hash[14] + hash[8] + hash[12] + hash[6] / 5;
+        final int b = hash[5] + hash[9] + hash[3] + hash[7] + hash[1] / 5;
+        return Color.argb(255, r, g, b);
+    }
+
+    private static void drawSquare(Canvas canvas, int x, int y, int size, Paint paint) {
+        final int yOffset = y * size;
+        final int xOffset = x * size;
+        canvas.drawRect(xOffset, yOffset, xOffset + size, yOffset + size, paint);
+    }
+}
+

winga456/frameworks_base

Change-Id: Ie0ffc4a4c2a41d6a5f35aad761c4c17a725bbc2a
Conflicts:
	core/java/android/provider/Settings.java

core/java/com/android/internal/util/vrtoxin/identicons/DotMatrixIdenticon.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.vrtoxin.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextUtils;
+
+import java.security.MessageDigest;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class DotMatrixIdenticon extends Identicon {
+    @Override
+    public Bitmap generateIdenticonBitmap(byte[] hash) {
+        if (hash.length < 16)
+            return null;
+
+        Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawColor(mBackgroundColor);
+
+        int blue = (hash[13] & 0x01f) << 3;
+        int green = (hash[14] & 0x01f) << 3;
+        int red = (hash[15] & 0x01f) << 3;
+        int color = Color.rgb(red, green, blue);
+        if (getColorDistance(color, mBackgroundColor) <= 64.0) {
+            color = getComplementaryColor(color);
+        }
+
+        Paint p = new Paint();
+        p.setColor(color);
+        p.setStyle(Paint.Style.FILL_AND_STROKE);
+        p.setAntiAlias(true);
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+                final int index = y * 5 + x;
+                float radius;
+                if ((index & 1) == 0) {
+                    radius = hash[index/2] & 0x0F;
+                } else {
+                    radius = (hash[index/2] >> 4) & 0x0F;
+                }
+                canvas.drawCircle(x * 16 + 8, y * 16 + 8, radius, p);
+            }
+        }
+        return bmp;
+    }
+
+    @Override
+    public byte[] generateIdenticonByteArray(byte[] hash) {
+        return bitmapToByteArray(generateIdenticonBitmap(hash));
+    }
+
+    @Override
+    public Bitmap generateIdenticonBitmap(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonBitmap(generateHash(saltedKey(key)));
+    }
+
+    @Override
+    public byte[] generateIdenticonByteArray(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonByteArray(generateHash(saltedKey(key)));
+    }
+}
+

core/java/com/android/internal/util/vrtoxin/identicons/IdenticonFactory.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.vrtoxin.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.provider.Settings;
+
+/**
+ * A factory to instantiate an identicon based on the type
+ */
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonFactory {
+
+    public static final int IDENTICON_STYLE_RETRO = 0;
+    public static final int IDENTICON_STYLE_CONTEMPORARY = 1;
+    public static final int IDENTICON_STYLE_SPIROGRAPH = 2;
+    public static final int IDENTICON_STYLE_DOTMATRIX = 3;
+
+    /**
+     * Get the appropriate identicon class based on the type passed in
+     * @param type
+     * @return
+     */
+    public static Identicon makeIdenticon(int type) {
+        switch (type) {
+            case IDENTICON_STYLE_RETRO:
+                return new RetroIdenticon();
+            case IDENTICON_STYLE_CONTEMPORARY:
+                return new NineBlockIdenticon();
+            case IDENTICON_STYLE_SPIROGRAPH:
+                return new SpirographIdenticon();
+            case IDENTICON_STYLE_DOTMATRIX:
+                return new DotMatrixIdenticon();
+            default:
+                throw new IllegalArgumentException("Unkown identicon type.");
+        }
+    }
+
+    /**
+     * Get the appropriate identicon class by looking up the user selected
+     * setting.
+     * @param context
+     * @return
+     */
+    public static Identicon makeIdenticon(Context context) {
+        return makeIdenticon(Settings.System.getInt(context.getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, 0));
+    }
+}
+

core/java/com/android/internal/util/vrtoxin/identicons/RetroIdenticon.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.vrtoxin.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class RetroIdenticon extends Identicon {
+
+    /**
+     * Generates a 5x5 identicon bitmap using the provided hash
+     * @param hash A 16 byte hash used to generate the identicon
+     * @return The bitmap of the identicon created
+     */
+    @Override
+    public Bitmap generateIdenticonBitmap(byte[] hash) {
+        if (hash.length != 16)
+            return null;
+
+        Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawColor(mBackgroundColor);
+        int color = generateColor(hash);
+        if (getColorDistance(color, mBackgroundColor) <= 64.0) {
+            color = getComplementaryColor(color);
+        }
+        Paint p = new Paint();
+        p.setColor(color);
+        p.setStyle(Paint.Style.FILL_AND_STROKE);
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 3; x++) {
+                final int index = y * 3 + x;
+                if (isOddParity(hash[index])) {
+                    drawSquare(canvas, x, y, 16, p);
+                    if (x == 0) drawSquare(canvas, 4, y, 16, p);
+                    if (x == 1) drawSquare(canvas, 3, y, 16, p);
+                }
+            }
+        }
+        return bmp;
+    }
+
+    /**
+     * Generates a 5x5 identicon bitmap, as a byte array, using the provided hash
+     * @param hash A 16 byte hash used to generate the identicon
+     * @return The bitmap byte array of the identicon created
+     */
+    @Override
+    public byte[] generateIdenticonByteArray(byte[] hash) {
+        return bitmapToByteArray(generateIdenticonBitmap(hash));
+    }
+
+    /**
+     * Generates an identicon bitmap using the provided key to generate a hash
+     * @param key A non empty string used to generate a hash when creating the identicon
+     * @return The bitmap of the identicon created
+     */
+    @Override
+    public Bitmap generateIdenticonBitmap(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonBitmap(generateHash(saltedKey(key)));
+    }
+
+    /**
+     * Generates an identicon bitmap, as a byte array, using the provided key to generate a hash
+     * @param key A non empty string used to generate a hash when creating the identicon
+     * @return The bitmap byte array of the identicon created
+     */
+    @Override
+    public byte[] generateIdenticonByteArray(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        return generateIdenticonByteArray(generateHash(saltedKey(key)));
+    }
+
+    private static boolean isOddParity(final byte b) {
+        int bb = b;
+        int bitCount = 0;
+        for (int i = 0; i < 8; i++, bb >>= 1) {
+            if ((bb & 1) != 0) {
+                bitCount++;
+            }
+        }
+
+        return (bitCount & 1) != 0;
+    }
+
+    private static int generateColor(byte[] hash) {
+        if (hash.length != 16)
+            return 0xFF000000;
+        final int r = hash[15] + hash[4] + hash[13] + hash[2] + hash[11] / 5;
+        final int g = hash[10] + hash[14] + hash[8] + hash[12] + hash[6] / 5;
+        final int b = hash[5] + hash[9] + hash[3] + hash[7] + hash[1] / 5;
+        return Color.argb(255, r, g, b);
+    }
+
+    private static void drawSquare(Canvas canvas, int x, int y, int size, Paint paint) {
+        final int yOffset = y * size;
+        final int xOffset = x * size;
+        canvas.drawRect(xOffset, yOffset, xOffset + size, yOffset + size, paint);
+    }
+}
+