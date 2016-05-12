7b30f798fdaf7136530492827c5a593ad64b8589
Svetoslav <svetoslavganov@google.com>
2016-05-12 03:06:01 -0400

    Fix default app for default grants
    Change-Id: I0419f3de0c7490d85244257901201021b252a432
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -259,7 +259,6 @@ final class DefaultPermissionGrantPolicy {
                     && doesPackageSupportRuntimePermissions(setupPackage)) {
                 grantRuntimePermissionsLPw(setupPackage, PHONE_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(setupPackage, CONTACTS_PERMISSIONS, userId);
-
             }
 
             // Camera
