c3bfff6730aacc7a1122a8282674a4be9389a64f
LorDClockaN <lordclockan@gmail.com>
2016-06-25 17:51:48 +0200

    FWB: QS shade stroke dash (1/2)
    Change-Id: Ic93dd299ea21649bc77c9c54e8d878fd76bc3869
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -252,6 +252,8 @@ public class NotificationPanelView extends PanelView implements
     private int mCustomStrokeColor;
     private int mCustomStrokeThickness;
     private int mCustomCornerRadius;
+    private int mCustomDashWidth;
+    private int mCustomDashGap;
 
     public NotificationPanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -2625,6 +2627,12 @@ public class NotificationPanelView extends PanelView implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_CORNER_RADIUS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_STROKE_DASH_WIDTH),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_STROKE_DASH_GAP),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -2687,6 +2695,10 @@ public class NotificationPanelView extends PanelView implements
                         Settings.System.QS_STROKE_THICKNESS, 4);
             mCustomCornerRadius = Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.QS_CORNER_RADIUS, 0);
+            mCustomDashWidth = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.QS_STROKE_DASH_WIDTH, 0);
+            mCustomDashGap = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.QS_STROKE_DASH_GAP, 10);
 
             setQSStroke();
             setQSType();
@@ -2734,10 +2746,11 @@ public class NotificationPanelView extends PanelView implements
                 mQsContainer.setBackground(qSGd);
             } else if (mQSStroke == 1) { // use accent color for border
                 qSGd.setColor(bgColor);
-                qSGd.setStroke(mCustomStrokeThickness, mContext.getResources().getColor(R.color.system_accent_color));
+                qSGd.setStroke(mCustomStrokeThickness, mContext.getResources().getColor(R.color.system_accent_color),
+                        mCustomDashWidth, mCustomDashGap);
             } else if (mQSStroke == 2) { // use custom border color
                 qSGd.setColor(bgColor);
-                qSGd.setStroke(mCustomStrokeThickness, mCustomStrokeColor);
+                qSGd.setStroke(mCustomStrokeThickness, mCustomStrokeColor, mCustomDashWidth, mCustomDashGap);
             }
 
             if (mQSStroke != 0) {
